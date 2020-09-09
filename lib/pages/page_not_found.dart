6320829752218ca4dbdfa8cef7acf22c2de58e99
CpdnCristiano <Cristiano@proserv.net.br>
2020-09-09 16:12:57 -0300

    fix 404

--- lib/pages/page_not_found.dart
+++ lib/pages/page_not_found.dart
@@ -1,7 +1,7 @@
 import 'package:api_cidades_br/widget/404_error.dart';
 import 'package:get_server/get_server.dart';
 
-class PageNotFoud extends GetView {
+class PageNotFound extends GetView {
   @override
   build(Context context) {
     return Error404(context, erro: 'Url não encontrada');
--- lib/widget/404_error.dart
+++ lib/widget/404_error.dart
@@ -2,5 +2,5 @@ import 'package:get_server/get_server.dart';
 
 class Error404 extends CustomResponse {
   Error404(Context context, {String erro = 'Página não encontrada'})
-      : super(() => context.response.status(4004).send(erro)) {}
+      : super(() => context.response.status(404).send(erro)) {}
 }
