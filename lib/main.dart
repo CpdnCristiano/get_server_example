e4b93388713a81a002c8572356327fc26aba3a72
Cristiano Nascimento <cristiano@proserv.net.br>
2021-08-07 21:43:04 -0300

    set port

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:api_cidades_br/pages/page_not_found.dart';
 import 'package:api_cidades_br/routes/app_pages.dart';
 import 'package:get_server/get_server.dart';
 
 void main() {
   runApp(GetServer(
-    port: 8000,
+    port: int.tryParse(Platform.environment['PORT'] ?? '8000'),
     getPages: AppPages.routes,
     onNotFound: PageNotFound(),
   ));
